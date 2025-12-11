669786dded3e70d255fc56e9d958d5c64ea4c9f4
GyanCarlosMateus <56090508+GyanCarlosMateus@users.noreply.github.com>
2025-12-11 09:08:32 -0300

    Classe Administrador
    Criação da classe de inicialização com os atributos

---
+++ Administrador.py
@@ -0,0 +1,8 @@
+class Administrador:
+    def __init__(self, id_adm: int, nome: str, email: str, login: str, senha: str):
+
+        self.id_adm = id_adm
+        self.nome = nome
+        self.email = email
+        self.login = login
+        self.senha = senha
