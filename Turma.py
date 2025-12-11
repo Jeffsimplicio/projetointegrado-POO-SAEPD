c9e5bd8d5048564b3819833e563843d1d403996e
GyanCarlosMateus <56090508+GyanCarlosMateus@users.noreply.github.com>
2025-12-11 09:27:58 -0300

    Classe Turma
    Criação da Classe Turma com os atributos de inicialização

---
+++ Turma.py
@@ -0,0 +1,8 @@
+class Turma:
+    def __init__(self, id_turma: int, nome: str, serie: str, turno: str, id_professor: int):
+        self.id_turma = id_turma
+        self.nome = nome
+        self.serie = serie
+        self.turno = turno
+        self.id_professor = id_professor
+        
